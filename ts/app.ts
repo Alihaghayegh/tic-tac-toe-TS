809658fcf04c59ba9f72edda02fc4c01f73d246f
alihaghayegh <haghayeghali95@gmail.com>
2023-09-28 19:07:19 +0330

    typescript configuration and folder structure and files in js form added to TS

---
+++ ts/app.ts
@@ -0,0 +1,58 @@
+import Store from "./store.js";
+import View from "./view.js";
+
+const players = [
+  {
+    id: 1,
+    name: "Player 1",
+    iconClass: "fa-x",
+    colorClass: "turquoise",
+  },
+  {
+    id: 2,
+    name: "Player 2",
+    iconClass: "fa-o",
+    colorClass: "yellow",
+  },
+];
+
+function init() {
+  const view = new View();
+  const store = new Store("live-t3-storage-key", players);
+
+  console.log(store.game);
+
+  window.addEventListener("storage", () => {
+    console.log("State changed from another tab");
+    view.render(store.game, store.stats);
+  });
+
+  // initView();
+  view.render(store.game, store.stats);
+
+  view.bindGameResetEvent((event) => {
+    store.reset();
+    view.render(store.game, store.stats);
+  });
+
+  view.bindNewRoundEvent((event) => {
+    store.newRound();
+    view.render(store.game, store.stats);
+  });
+
+  view.bindPlayerMoveEvent((square) => {
+    const existingMove = store.game.moves.find(
+      (move) => move.squareId === +square.id
+    );
+
+    if (existingMove) {
+      return;
+    }
+
+    store.playeMove(+square.id);
+
+    view.render(store.game, store.stats);
+  });
+}
+
+window.addEventListener("load", init);
